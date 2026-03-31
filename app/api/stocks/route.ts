69be825793d9603e70032f6cb1334a7a2bb658bf
Your Name <gabriel.dauer@outlook.com>
2026-03-30 23:53:51 -0300

    fix: add brapi token

--- app/api/stocks/route.ts
+++ app/api/stocks/route.ts
@@ -1,11 +1,15 @@
 import { NextResponse } from "next/server"
 
-export async function GET(req:Request){
-
+export async function GET(req: Request) {
   const { searchParams } = new URL(req.url)
   const ticker = searchParams.get("ticker")
 
-  const r = await fetch(`https://brapi.dev/api/quote/${ticker}`)
+  const token = process.env.BRAPI_TOKEN
+
+  const r = await fetch(
+    `https://brapi.dev/api/quote/${ticker}?token=${token}`
+  )
+
   const data = await r.json()
 
   return NextResponse.json(data)
